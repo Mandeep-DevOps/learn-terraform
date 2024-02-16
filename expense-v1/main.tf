3c40d35bd84333c174e1badcc8b439ec11cee419
home <self@self.com>
2024-02-16 20:37:56 +0530

    Terraform Learnings.

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
-  ami                    = "ami-0f3c7d07486cad139"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0512018371fd501d7"]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = "frontend"
@@ -9,7 +9,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z09069432YIRFR4VG7EKK"
+  zone_id = var.zone_id
   name    = "frontend"
   type    = "A"
   ttl     = 30
@@ -18,9 +18,9 @@ resource "aws_route53_record" "frontend" {
 
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-0f3c7d07486cad139"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0512018371fd501d7"]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = "backend"
@@ -28,7 +28,7 @@ resource "aws_instance" "backend" {
 }
 
 resource "aws_route53_record" "backend" {
-  zone_id = "Z09069432YIRFR4VG7EKK"
+  zone_id = var.zone_id
   name    = "backend"
   type    = "A"
   ttl     = 30
@@ -36,9 +36,9 @@ resource "aws_route53_record" "backend" {
 }
 
 resource "aws_instance" "mysql" {
-  ami                    = "ami-0f3c7d07486cad139"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0512018371fd501d7"]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = "mysql"
@@ -46,7 +46,7 @@ resource "aws_instance" "mysql" {
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "Z09069432YIRFR4VG7EKK"
+  zone_id = var.zone_id
   name    = "mysql"
   type    = "A"
   ttl     = 30
---
+++ expense-v1/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0512018371fd501d7"]
+}
+
+variable "zone_id" {
+  default = "Z09069432YIRFR4VG7EKK"
+}
