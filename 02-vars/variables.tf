35ebf0c23d1ac99dbdfa7a3b5adafef4d9bb0d33
home <self@self.com>
2024-02-16 17:35:15 +0530

    Terraform Learnings.

--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,16 @@
 variable "v1" {
   default = "Hello Moto"
 }
+
+variable 'v2' {
+  default = [ "Hello Moto Moto", 100, true]
+}
+## A variable list can have different data types.
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Two Hundred"
+  }
+}
+
