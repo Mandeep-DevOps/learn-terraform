bb77dbfff6d706ebef4629abf48e25a956de24c8
home <self@self.com>
2024-02-16 21:39:08 +0530

    Terraform Learnings.

---
+++ expense-v2/main.tf
@@ -0,0 +1,55 @@
+resource "aws_instance" "frontend" {
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = var.zone_id
+  name    = "frontend"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
+resource "aws_instance" "backend" {
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = var.zone_id
+  name    = "backend"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
---
+++ expense-v2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0512018371fd501d7"]
+}
+
+variable "zone_id" {
+  default = "Z09069432YIRFR4VG7EKK"
+}
