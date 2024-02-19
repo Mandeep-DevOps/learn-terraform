fcd70346789786125efee04755d9187754f692f0
home <self@self.com>
2024-02-19 14:50:12 +0530

    Terraform Learnings.

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -2,7 +2,9 @@ resource "null_resource" "test" {
   count = 10
 }
 
+# By providing count, resource will become list.
+
 output "test" {
-  value = null_resource.test[0].id
+  value = null_resource.test.*.id
 }
 
