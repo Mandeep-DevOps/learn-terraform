456165f7d70ca9e552c837dd6a224aad2a9ceaef
home <self@self.com>
2024-02-19 17:18:05 +0530

    Terraform Learnings.

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -8,3 +8,28 @@ output "test" {
   value = null_resource.test.*.id
 }
 
+#resource "aws_instance" "test-test" {
+#  count                  = 3
+#  ami                    = "ami-0f3c7d07486cad139"
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = ["sg-0512018371fd501d7"]
+#
+#  tags = {
+#    Name = "test-test"
+#  }
+#}
+
+variable "components" {
+  default = [ "frontend", "backend", "mysql"]
+}
+
+resource "aws_instance" "test-test" {
+  count                  = length(var.components)
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0512018371fd501d7"]
+
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
