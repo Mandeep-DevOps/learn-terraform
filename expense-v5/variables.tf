ea551182ec1a0303e6bfa84eaf87dea499bbb4da
home <self@self.com>
2024-02-20 18:00:48 +0530

    Terraform Learnings.

---
+++ expense-v5/variables.tf
@@ -0,0 +1,26 @@
+variable "components" {
+  default           = {
+    frontend        = {
+      name          = "frontend"
+      instance_type = "t3.micro"
+    }
+    mysql           = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    backend = {
+      name = "backend"
+      instance_type = "t3.micro"
+    }
+
+    }
+  }
+}
+
+variable "zone_id" {
+  default = "Z09069432YIRFR4VG7EKK"
+}
+
+variable "vpc_security_group_ids" {
+default = ["sg-0512018371fd501d7"]
+}
